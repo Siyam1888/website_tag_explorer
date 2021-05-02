6779acb26f29d803ce39a3098951ded51d35efbe
Siyam Ahmed <siyamraisa@gmail.com>
2021-05-02 07:18:33 +0600

    Added new tags and beacon scanning feature

--- dump_tags.py
+++ dump_tags.py
@@ -1,9 +1,11 @@
 from openpyxl import load_workbook
 import json
+import webbrowser
 
 
-file_path = 'Tags.xlsx'
+file_path = "Tags.xlsx"
 wb = load_workbook(file_path)
+
 
 def dump_tags():
     """Read the tag names and tag urls and save the list of their pairs as json"""
@@ -15,12 +17,22 @@ def dump_tags():
         tag_name = inputs[f"A{row}"].value
         tag_url = inputs[f"C{row}"].value
         if tag_name and tag_url:
-            tag = {'tag_name': tag_name, 'tag_urls': tag_url.strip().split(',')}
+            tag = {
+                "tag_name": tag_name,
+                "tag_urls": list(
+                    filter(
+                        lambda x: x != "",
+                        map(
+                            lambda x: x.replace("https://", "").strip(),
+                            tag_url.strip().split(","),
+                        ),
+                    )
+                ),
+            }
             tags_list.append(tag)
 
-    with open('tags_list.json', 'w') as f:
+    with open("tags_list.json", "w") as f:
         json.dump(tags_list, f)
 
 
 dump_tags()
-
--- tag_explorer.py
+++ tag_explorer.py
@@ -20,9 +20,10 @@ class WebsiteTagExplorer:
             output_dict = json.loads(json_output)
 
             return output_dict
-    
-    def get_hosts(self, url, output):
-        hosts = output['hosts']
+
+    def get_hosts(self, output):
+        """Read the hosts and return the unique ones"""
+        hosts = output["hosts"]
         return_values = set()
 
         # iterating over the keys of hosts e.g requests, beacons, cookies, links
@@ -35,9 +36,19 @@ class WebsiteTagExplorer:
                     # return_values.append(value)
 
                     # adding only third party links
-                    if link_type == 'thirdParty':
+                    if link_type == "thirdParty":
                         return_values.add(link)
-        
+
+        return return_values
+
+    def get_beacons(self, output):
+        """Read the beacons from the given website-evidence-collector output and return a set"""
+        beacons = output["beacons"]
+        return_values = set()
+
+        for beacon in beacons:
+            return_values.add(beacon["url"])
+
         return return_values
 
 
@@ -56,6 +67,11 @@ class Excel:
             self.wb.create_sheet("Tags")
             if "Tags" not in self.wb.sheetnames
             else self.wb["Tags"]
+        )
+        self.errors = (
+            self.wb.create_sheet("Errors")
+            if "Errors" not in self.wb.sheetnames
+            else self.wb["Errors"]
         )
 
     def make_columns(self, cells_zip, sheet, general_width=20, url_width=50):
@@ -102,71 +118,87 @@ class Excel:
             if value := inputs[f"A{row}"].value:
                 yield value
 
-
     def append_tags(self, tags):
         """Read the hosts, host categories, link types, links and append them to excel"""
 
         for host in tags:
-            self.wb['Tags'].append(
+            self.wb["Tags"].append(
                 (
-                    host['site'],
-                    host['tag_name'],
-                    host['tag_url'],
+                    host["site"],
+                    host["tag_name"],
+                    host["tag_url"],
                 )
             )
 
             self.wb.save(self.file_name)
-        
 
 
-
-def get_output(url):
+def get_final_output(url):
     """Takes an URL and returns all the tags found on the url -> tag_name, tag_url"""
     tag_explorer = WebsiteTagExplorer()
 
     # Getting the output from the website evidence collector
     output = tag_explorer.get_output(url)
     if output:
-        hosts = tag_explorer.get_hosts(url, output)
-        # Loading the list of tag names and corresponding urls from the json 
-        with open('tags_list.json') as f:
+        beacons = tag_explorer.get_beacons(output)
+        hosts = tag_explorer.get_hosts(output)
+        # Loading the list of tag names and corresponding urls from the json
+        with open("tags_list.json") as f:
             tags_list = json.load(f)
 
         # list of dictionaries to return
         tags_found = []
         named_tags = set()
+        found_beacons = set()
 
         # Naming the tags from url and adding them to the list
         for tag in tags_list:
-            for tag_url in tag['tag_urls']:
+
+            for tag_url in tag["tag_urls"]:
+                # matching beacon urls
+                for beacon_url in beacons:
+                    if tag_url in beacon_url:
+                        tags_found.append(
+                            {
+                                "site": url,
+                                "tag_name": tag["tag_name"],
+                                "tag_url": beacon_url,
+                            }
+                        )
+                        found_beacons.add(tag["tag_name"])
+
+                # matching hosts
                 for host in hosts:
-                    if host in tag_url:
+                    if (host in tag_url) or (tag_url in host):
                         # print(tag['tag_name'], url, host)
-                        tags_found.append({'site': url, 'tag_name': tag['tag_name'], 'tag_url': host})
+
+                        # if tag['tag_name'] not in found_beacons:
+                        tags_found.append(
+                            {"site": url, "tag_name": tag["tag_name"], "tag_url": host}
+                        )
                         named_tags.add(host)
-        
+
         # Adding the unnamed tags
-        for host in (hosts - named_tags):
-            tags_found.append({'site': url, 'tag_name': None, 'tag_url': host})
+        for host in hosts - named_tags:
+            tags_found.append({"site": url, "tag_name": None, "tag_url": host})
 
         return tags_found
 
-
-                        
-
-    print(f'{url} : The evidence collector did not return anything :(')
-
+    print(f"{url} : The evidence collector did not return anything :(")
 
 
 def main():
-    filename = 'Output.xlsx'
+    filename = "Output.xlsx"
     excel = Excel(filename)
     for url in excel.generate_inputs():
-        tags = get_output(url)
+        tags = get_final_output(url)
         # print(tags)
         if tags:
             excel.append_tags(tags)
+        else:
+            excel.errors.append((url,))
 
     excel.wb.save(filename)
+
 
 main()
